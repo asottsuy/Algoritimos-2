9dad6cd61071de9353452d8b09de2f1b3b353a70
Felipe Tatsuya Aso <asotatsuya0304@gmail.com>
2025-07-02 21:24:01 -0300

    implementado a função de inserir com rotações simples e duplas, calculo da altura do nó e fator de balanceamento

--- Avaliacoes/AVL - avaliação final/AVL.py
+++ Avaliacoes/AVL - avaliação final/AVL.py
@@ -1,5 +1,4 @@
-from turtle import filling
-
+from No import No
 
 class AVL:
     def __init__(self):
@@ -9,14 +8,14 @@ class AVL:
         if not nodo:
             return 0
         return nodo.altura
-    
+        
     def _get_fator_balanceamento(self, nodo):
         if not nodo:
             return 0
         # Fator = Altura da Direita - Altura da Esquerda
         return self._get_altura(nodo.direita) - self._get_altura(nodo.esquerda)
             
-    def _rotacao_direita(self, pai): #fator < -1
+    def _rotacao_direita(self, pai): #fator < -1/
         filhoE = pai.esquerda
         neto = filhoE.direita
         filhoE.direita = pai
@@ -39,3 +38,39 @@ class AVL:
         filhoD = 1 + max(self._get_altura(filhoD.esquerda), self._get_altura(filhoD.direita))
 
         return filhoD
+
+    def inserir(self, valor):
+        if self.raiz is None:
+            self.raiz = No(valor)
+            
+        else:
+            print(f"-----Inserindo Nó folha: {valor}-----")
+            self.raiz = self._inserir_no_folha(self.raiz, valor)
+
+    def _inserir_no_folha(self, no_atual, valor):
+        if not no_atual:
+            return No(valor)
+        elif valor < no_atual:
+            no_atual.esquerda = self._inserir_no_folha(no_atual.esquerda, valor)
+        else:
+            no_atual.direita = self._inserir_no_folha(no_atual.direita, valor)
+
+        #calcular a altura
+        no_atual.altura = 1 + max(self._get_altura(no_atual.esquerda), self._get_altura(no_atual.direita))
+
+        fator = self._get_fator_balanceamento(no_atual)
+
+        if fator < -1:
+            if valor < no_atual.esquerda.valor: #rotação simples para a direita
+                self._rotacao_direita(no_atual)
+            else: #rotação dupla 1) esquerda 2)direita
+                no_atual.esquerda = self._rotacao_esquerda(no_atual.esquerda)
+                self._rotacao_direita(no_atual)   
+                
+        elif fator > 1:
+            if valor < no_atual.direita.valor: #rotação simples para a esquerda
+                self._rotacao_esquerda(no_atual)
+            else: #rotação dupla 1)direita 2)esquerda
+                no_atual.direita = self._rotacao_direita(no_atual.direita)
+                self._rotacao_esquerda(no_atual)
+        return no_atual
